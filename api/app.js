f0321f816be3fac497c4a925b64526f334409e80
Theo <tsaridakis@gmail.com>
2024-04-15 01:36:24 +0300

    Add Protected Routes functionality and a Test route

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
+import testRoute from "./routes/test.route.js";
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/test", testRoute);
 
 app.listen(8800, () => {
   console.log("Server is running on port 8800!");
